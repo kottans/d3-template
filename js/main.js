dd4920358a56600e4a8ae11a7a2ca9ebe0ce712e
sudodoki <smd.deluzion@gmail.com>
2014-12-06 10:16:13 +0200

    Add class toggling & simple height function

--- js/main.js
+++ js/main.js
@@ -7,11 +7,17 @@ d3.json('./dataset.json', function (err, data) {
   var top10 = _(massagedData)
                 .sortBy(function (item) { return - parseInt(item.total, 10); })
                 .first(10)
+                .shuffle()
                 .value();
   d3
     .select('body').selectAll('div')
     .data(top10)
     .enter()
     .append('div')
-    .text(function (d) { return d.id + ': ' + d.total; });
+    .attr('class', function (d, i) {
+      return (i % 2) ? 'bar' : 'bar special';
+    })
+    .style('height', function (d) {
+      return d.total * 2 + 'px';
+    })
 });
