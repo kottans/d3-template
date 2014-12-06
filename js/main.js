d4d00b99b42256f87fd78bca30359e655abee346
sudodoki <smd.deluzion@gmail.com>
2014-12-06 09:52:46 +0200

    Get massaged data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('hello world!');
+
+d3.json('./dataset.json', function (err, data) {
+  var massagedData = _.transform(data.medals, function (result, val, key) {
+    result.push(_.assign({id: key}, val[0]));
+    return result;
+  }, []);
+  console.log(massagedData);
+});
